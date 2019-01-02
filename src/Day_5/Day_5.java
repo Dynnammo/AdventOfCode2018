bee9552596029f5b6914981b24f9dc3fc5c783bd
dynnammo <dynnammo@dynnammo>
2019-01-02 10:53:02 +0100

    Finish Day 5 problem

--- src/Day_5/Day_5.java
+++ src/Day_5/Day_5.java
@@ -16,15 +16,6 @@ public class Day_5 {
     final int GAP = 32;
     public static void main(String[] args) throws FileNotFoundException {
         Polymer polymer = new Polymer(System.getProperty("user.dir")+ "/src/Day_5/Polymer.txt");
-//        String chain = "";
-//        for (int i = 0; i < polymer.chain.length()/5000; i++)
-//            chain += polymer.simulateReaction(polymer.chain.substring(5000*i, 5000*(i+1)));
-//        chain = polymer.simulateReaction(chain);
-//        System.out.println(chain.length());
-
-        polymer.simulateFullReaction();
-        System.out.println(polymer.chain.length());
+        System.out.println(polymer.getImprovedPolymer());
     }
-    
-    
 }
